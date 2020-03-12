6583e17b847dc68912cf6ac094dcf2f4ccfea14e
AnHardt <github@kitelab.de>
2020-03-12 18:23:06 +0100

    Normalize resistance before calculating positive temperature
    Normalize resistance before calculating temperature with the formula for the positive temperatures.
    Positive temperature means (when normalized) resistance > 100 Ohm.
    We then can decide what formula to use, before we calculated the positive one.
    
    This should significantly speed up the calculation for negative temperatures.
    
    Even more efficient would be to store the normalized value for Rref in the object - but that would change the API.

--- Adafruit_MAX31865.cpp
+++ Adafruit_MAX31865.cpp
@@ -210,34 +210,31 @@ float Adafruit_MAX31865::temperature(float RTDnominal, float refResistor) {
 
   // Serial.print("\nResistance: "); Serial.println(Rt, 8);
 
-  Z1 = -RTD_A;
-  Z2 = RTD_A * RTD_A - (4 * RTD_B);
-  Z3 = (4 * RTD_B) / RTDnominal;
-  Z4 = 2 * RTD_B;
+  // first, if needed, normalize to 100 ohm
+  if (RTDnominal != 100.0) Rt *= 100.0/RTDnominal;
 
-  temp = Z2 + (Z3 * Rt);
-  temp = (sqrt(temp) + Z1) / Z4;
+  if (Rt >= 100.0) { // above 100 Ohm temperature will become positive
+    Z1 = -RTD_A;
+    Z2 = RTD_A * RTD_A - (4 * RTD_B);
+    Z3 = (4 * RTD_B) / 100.0;
+    Z4 = 2 * RTD_B;
 
-  if (temp >= 0)
-    return temp;
+    temp = Z2 + (Z3 * Rt);
+    temp = (sqrt(temp) + Z1) / Z4;
+  }  else {
+    float rpoly = Rt;
 
-  // ugh.
-  Rt /= RTDnominal;
-  Rt *= 100; // normalize to 100 ohm
-
-  float rpoly = Rt;
-
-  temp = -242.02;
-  temp += 2.2228 * rpoly;
-  rpoly *= Rt; // square
-  temp += 2.5859e-3 * rpoly;
-  rpoly *= Rt; // ^3
-  temp -= 4.8260e-6 * rpoly;
-  rpoly *= Rt; // ^4
-  temp -= 2.8183e-8 * rpoly;
-  rpoly *= Rt; // ^5
-  temp += 1.5243e-10 * rpoly;
-
+    temp = -242.02;
+    temp += 2.2228 * rpoly;
+    rpoly *= Rt; // square
+    temp += 2.5859e-3 * rpoly;
+    rpoly *= Rt; // ^3
+    temp -= 4.8260e-6 * rpoly;
+    rpoly *= Rt; // ^4
+    temp -= 2.8183e-8 * rpoly;
+    rpoly *= Rt; // ^5
+    temp += 1.5243e-10 * rpoly;
+  }
   return temp;
 }
 
