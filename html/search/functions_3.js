dab6493dc987921bdbb24bba80b5f0627906f89e
Doxygen CI <travis@travis-ci.org>
2020-05-11 18:38:29 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['enable50hz',['enable50Hz',['../class_adafruit___m_a_x31865.html#af0a806a739f93b371a4edfb99a63ef9a',1,'Adafruit_MAX31865']]],
   ['enablebias',['enableBias',['../class_adafruit___m_a_x31865.html#a402d233283efda0e0a87ed5b37d5c031',1,'Adafruit_MAX31865']]]
 ];
