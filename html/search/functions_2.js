c6d5815b7cbd02bb5703ff764757496e09d1c93c
Doxygen CI <ci-arduino@invalid>
2022-06-21 16:27:31 +0000

    Deploy docs to GitHub Pages from commit 4fecc8ca98
    Commit: 4fecc8ca98d55616e424467ba5f697fb7804efc1
    GitHub Actions run: 2536836829

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['calculatetemperature',['calculateTemperature',['../class_adafruit___m_a_x31865.html#af19f4cb4fadc6529eee1036c2d95d383',1,'Adafruit_MAX31865']]],
   ['clearfault',['clearFault',['../class_adafruit___m_a_x31865.html#aac7b4480b0824e44bb89c4f862e5419d',1,'Adafruit_MAX31865']]]
 ];
