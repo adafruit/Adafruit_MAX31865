94a54d4bd43a2f632f1eeb926ad0459af35cebc6
Doxygen CI <ci-arduino@invalid>
2022-06-28 21:20:22 +0000

    Deploy docs to GitHub Pages from commit f523720fb1
    Commit: f523720fb1c4fb076ed05cbb41afb18bc519d3b1
    GitHub Actions run: 2579193872

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['adafruit_5fmax31865',['Adafruit_MAX31865',['../class_adafruit___m_a_x31865.html',1,'Adafruit_MAX31865'],['../class_adafruit___m_a_x31865.html#ab2dfc513b027d4bb957d2588f4155368',1,'Adafruit_MAX31865::Adafruit_MAX31865(int8_t spi_cs, int8_t spi_mosi, int8_t spi_miso, int8_t spi_clk)'],['../class_adafruit___m_a_x31865.html#a92866ce4e53d241c6c0b56f433185eb2',1,'Adafruit_MAX31865::Adafruit_MAX31865(int8_t spi_cs)']]],
+  ['adafruit_5fmax31865',['Adafruit_MAX31865',['../class_adafruit___m_a_x31865.html',1,'Adafruit_MAX31865'],['../class_adafruit___m_a_x31865.html#ab2dfc513b027d4bb957d2588f4155368',1,'Adafruit_MAX31865::Adafruit_MAX31865(int8_t spi_cs, int8_t spi_mosi, int8_t spi_miso, int8_t spi_clk)'],['../class_adafruit___m_a_x31865.html#a5edd7ca73bf61592f26e1bdfc99749b3',1,'Adafruit_MAX31865::Adafruit_MAX31865(int8_t spi_cs, SPIClass *theSPI=&amp;SPI)']]],
   ['autoconvert',['autoConvert',['../class_adafruit___m_a_x31865.html#af0680dc55c043cc56b514fe65c2b9705',1,'Adafruit_MAX31865']]]
 ];
